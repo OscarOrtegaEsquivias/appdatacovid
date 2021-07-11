a4f021b8af0cf356f3cb19ff0d6e7a6ebfd42fc2
OscarOrtegaEsquivias <or73ga.es@gmail.com>
2021-07-10 22:08:42 -0500

    Primera Version

---
+++ PacienteData/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import *
+
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+# Register your models here.
+
+#admin.site.register(Paciente)
+
+class PacienrteResource(resources.ModelResource):
+	class Meta:
+		model = Paciente
+
+
+class PacienteAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+
+	search_fields = ['num_document','nombre','apePaterno','apeMaterno','sexo','celular']
+	list_display = ('num_document','nombre','apePaterno','apeMaterno','sexo','celular')
+#	ordering = ['dni']
+
+	resource_class = PacienrteResource
+
+
+admin.site.register(Paciente, PacienteAdmin)
---
+++ PacienteData/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from django_userforeignkey.models.fields import UserForeignKey
+
+
+class Paciente(models.Model):
+	tipo_document = (
+		('1','DNI'),
+		('2','Carnet de Extrangeria'),
+		)
+	tipo_doc = models.CharField(max_length=1, choices=tipo_document)
+	num_document = models.CharField(max_length=9)
+	nombre = models.CharField(max_length=100, blank=True)
+	apePaterno = models.CharField(max_length=100, blank=True)
+	apeMaterno = models.CharField(max_length=100, blank=True)
+	sexos = (
+		('0','FEMENINO'),
+		('1','MASCULINO'),
+		)
+	sexo = models.CharField(max_length=1, choices=sexos, blank=True)
+	celular = models.CharField(max_length=9, blank=True)
+	userDiresa  =  UserForeignKey(auto_user = True, related_name = "Paciente")
+	fechaPrimeraDosis = models.DateField(blank=True)
+	fechaSegundaDosis = models.DateField(blank=True)
+
+
+
+	def __str__(self):
+		return self.nombre
