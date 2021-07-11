a008eba92a87072ffad3f1520d4059903f14aa59
OscarOrtegaEsquivias <or73ga.es@gmail.com>
2021-07-11 16:04:06 -0500

    Configuracion DEBUG False

--- DataCovid/settings.py
+++ DataCovid/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mj35fl42v#zp*g(ps$_ip%tt!ebxgb^f7227tlgw_a=65-3b1#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
